55f7bf1c93afd7c9faa2d4766a4c1a3b5e839069
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-10-15 15:20:28 +0200

    feat: add set timer button / the progress bar / the clear button

--- TimeClock/Index.Designer.cs
+++ TimeClock/Index.Designer.cs
@@ -30,17 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
+            this.TimerTimer = new System.Windows.Forms.Timer(this.components);
             this.StartButton = new System.Windows.Forms.Button();
             this.ResetButton = new System.Windows.Forms.Button();
             this.ClearButton = new System.Windows.Forms.Button();
+            this.TimerLabel = new System.Windows.Forms.Label();
             this.TimerSeterTextBox = new System.Windows.Forms.TextBox();
             this.TimerProgessBar = new System.Windows.Forms.ProgressBar();
-            this.TimerLabel = new System.Windows.Forms.Label();
-            this.MainScreen = new System.Windows.Forms.GroupBox();
             this.SetTimerButton = new System.Windows.Forms.Button();
-            this.TimerTimer = new System.Windows.Forms.Timer(this.components);
-            this.MainScreen.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TimerTimer
+            // 
+            this.TimerTimer.Interval = 1000;
+            this.TimerTimer.Tick += new System.EventHandler(this.TimerTick);
             // 
             // StartButton
             // 
@@ -49,7 +52,7 @@
             this.StartButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartButton.Location = new System.Drawing.Point(158, 371);
+            this.StartButton.Location = new System.Drawing.Point(22, 215);
             this.StartButton.Name = "StartButton";
             this.StartButton.Size = new System.Drawing.Size(75, 23);
             this.StartButton.TabIndex = 1;
@@ -64,12 +67,13 @@
             this.ResetButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ResetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResetButton.Location = new System.Drawing.Point(418, 371);
+            this.ResetButton.Location = new System.Drawing.Point(282, 215);
             this.ResetButton.Name = "ResetButton";
             this.ResetButton.Size = new System.Drawing.Size(75, 23);
             this.ResetButton.TabIndex = 2;
             this.ResetButton.Text = "Reset";
             this.ResetButton.UseVisualStyleBackColor = false;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
             // ClearButton
             // 
@@ -78,17 +82,28 @@
             this.ClearButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClearButton.Location = new System.Drawing.Point(675, 371);
+            this.ClearButton.Location = new System.Drawing.Point(539, 215);
             this.ClearButton.Name = "ClearButton";
             this.ClearButton.Size = new System.Drawing.Size(75, 23);
             this.ClearButton.TabIndex = 3;
             this.ClearButton.Text = "Clear";
             this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 45.75F);
+            this.TimerLabel.Location = new System.Drawing.Point(183, 10);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(284, 71);
+            this.TimerLabel.TabIndex = 4;
+            this.TimerLabel.Text = "00:00:00";
             // 
             // TimerSeterTextBox
             // 
             this.TimerSeterTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.TimerSeterTextBox.Location = new System.Drawing.Point(418, 434);
+            this.TimerSeterTextBox.Location = new System.Drawing.Point(282, 278);
             this.TimerSeterTextBox.Name = "TimerSeterTextBox";
             this.TimerSeterTextBox.Size = new System.Drawing.Size(75, 23);
             this.TimerSeterTextBox.TabIndex = 5;
@@ -98,41 +113,15 @@
             // TimerProgessBar
             // 
             this.TimerProgessBar.ForeColor = System.Drawing.Color.Green;
-            this.TimerProgessBar.Location = new System.Drawing.Point(148, 307);
+            this.TimerProgessBar.Location = new System.Drawing.Point(12, 151);
             this.TimerProgessBar.Name = "TimerProgessBar";
             this.TimerProgessBar.Size = new System.Drawing.Size(613, 38);
             this.TimerProgessBar.TabIndex = 6;
             // 
-            // TimerLabel
-            // 
-            this.TimerLabel.AutoSize = true;
-            this.TimerLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 45.75F);
-            this.TimerLabel.Location = new System.Drawing.Point(319, 166);
-            this.TimerLabel.Name = "TimerLabel";
-            this.TimerLabel.Size = new System.Drawing.Size(284, 71);
-            this.TimerLabel.TabIndex = 4;
-            this.TimerLabel.Text = "00:00:00";
-            // 
-            // MainScreen
-            // 
-            this.MainScreen.Controls.Add(this.SetTimerButton);
-            this.MainScreen.Controls.Add(this.TimerProgessBar);
-            this.MainScreen.Controls.Add(this.TimerSeterTextBox);
-            this.MainScreen.Controls.Add(this.TimerLabel);
-            this.MainScreen.Controls.Add(this.ClearButton);
-            this.MainScreen.Controls.Add(this.ResetButton);
-            this.MainScreen.Controls.Add(this.StartButton);
-            this.MainScreen.Location = new System.Drawing.Point(12, 12);
-            this.MainScreen.Name = "MainScreen";
-            this.MainScreen.Size = new System.Drawing.Size(919, 482);
-            this.MainScreen.TabIndex = 7;
-            this.MainScreen.TabStop = false;
-            this.MainScreen.Text = "MainScreen";
-            // 
             // SetTimerButton
             // 
             this.SetTimerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SetTimerButton.Location = new System.Drawing.Point(499, 434);
+            this.SetTimerButton.Location = new System.Drawing.Point(363, 278);
             this.SetTimerButton.Name = "SetTimerButton";
             this.SetTimerButton.Size = new System.Drawing.Size(41, 23);
             this.SetTimerButton.TabIndex = 7;
@@ -140,38 +129,37 @@
             this.SetTimerButton.UseVisualStyleBackColor = true;
             this.SetTimerButton.Click += new System.EventHandler(this.SetTimerButton_Click);
             // 
-            // TimerTimer
-            // 
-            this.TimerTimer.Interval = 1000;
-            this.TimerTimer.Tick += new System.EventHandler(this.TimerTick);
-            // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(943, 506);
-            this.Controls.Add(this.MainScreen);
+            this.ClientSize = new System.Drawing.Size(651, 314);
+            this.Controls.Add(this.SetTimerButton);
+            this.Controls.Add(this.TimerProgessBar);
+            this.Controls.Add(this.TimerSeterTextBox);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.ClearButton);
             this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Index";
             this.Text = "Time Clock";
-            this.MainScreen.ResumeLayout(false);
-            this.MainScreen.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Timer TimerTimer;
         private System.Windows.Forms.Button StartButton;
         private System.Windows.Forms.Button ResetButton;
         private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label TimerLabel;
         private System.Windows.Forms.TextBox TimerSeterTextBox;
         private System.Windows.Forms.ProgressBar TimerProgessBar;
-        private System.Windows.Forms.Label TimerLabel;
-        private System.Windows.Forms.GroupBox MainScreen;
         private System.Windows.Forms.Button SetTimerButton;
-        private System.Windows.Forms.Timer TimerTimer;
     }
 }
 
--- TimeClock/Index.cs
+++ TimeClock/Index.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace TimeClock
 {
@@ -23,8 +18,11 @@ namespace TimeClock
         {
             TimeSpan time = TimeSpan.FromSeconds(remainingTime);
             TimerLabel.Text = time.ToString(@"hh\:mm\:ss");
+            TimerProgessBar.Minimum = 0;
+            TimerProgessBar.Maximum = setTime;
+            TimerProgessBar.Value = remainingTime;
         }
-        private void TimerTick(object sender, EventArgs e) //method tu update the timer inside of the componene
+        private void TimerTick(object sender, EventArgs e) //method to update the timer inside of the componene
         {
             if (remainingTime > 0) 
             {
@@ -33,34 +31,95 @@ namespace TimeClock
             }
             else
             {
-                TimerTimer.Stop();
-                MessageBox.Show("Time is up!"),
+                StartButton_Click(sender, e);
+                SystemSounds.Beep.Play();
+                MessageBox.Show("Time is up!");
             }
         }
 
-        private void StartButton_Click(object sender, EventArgs e)
+        private void StartButton_Click(object sender, EventArgs e)// method to do a flip flop button start/stop and start and stop the timer
         {
             if (!isTimerRunning)
             {
                 TimerTimer.Start();
+                isTimerRunning = true;
                 StartButton.Text = "Stop";
                 StartButton.BackColor = Color.Blue;
             }
             else
             {
                 TimerTimer.Stop();
+                isTimerRunning = false;
                 StartButton.Text = "Start";
                 StartButton.BackColor = Color.Chartreuse;
             }
         }
 
-        private void SetTimerButton_Click(object sender, EventArgs e)
+        private void SetTimerButton_Click(object sender, EventArgs e)//method to set the timer and check if the entry is valid
         {
-            IEnumerable<string> texteInputSplited = TimerSeterTextBox.Text.Split(':');
-            if (int.TryParse(, out int value))
-            {
+            string[] setTimmerSplitedFromTextBox = TimerSeterTextBox.Text.Split(':');
+            
 
+            if (setTimmerSplitedFromTextBox.Length != 3)
+            {
+                MessageBox.Show("Please enter in the valid format (HH:MM:SS)");
+                return;
             }
+            else
+            {
+                //check if the entry are number and can be parsed and store the return in a bool local variable
+                bool hoursParsed , minutesParsed , secondsParsed ;
+                hoursParsed = int.TryParse(setTimmerSplitedFromTextBox[0], out int hours);
+                minutesParsed = int.TryParse(setTimmerSplitedFromTextBox[1], out int minutes);
+                secondsParsed = int.TryParse(setTimmerSplitedFromTextBox[2], out int seconds);
+
+                if (hours > 100 || minutes > 60 || seconds > 60) //check if the number we tried to set does not exceed the format
+                {
+                    MessageBox.Show("Please enter in the valid format (HH:MM:SS)");
+                    return;
+                }
+
+                if (hoursParsed && minutesParsed && secondsParsed)// check if all the local variable for the parsing are true
+                {
+                    setTime = hours * 3600 + minutes * 60 + seconds;
+                    remainingTime = hours * 3600 + minutes * 60 + seconds;
+                    UpdateTimer();
+
+                    if(isTimerRunning)
+                    {
+                        StartButton_Click(sender, e);
+                    }
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Please enter in the valid format (HH:MM:SS)");
+                    return;
+                }
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e) //method to reset the timer to the start of it
+        {
+            if (isTimerRunning)
+            {
+                StartButton_Click(sender, e);
+            }
+            remainingTime = setTime;
+            UpdateTimer() ;
+            
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)//method to clear the timer and the set textbox part of the app
+        {
+            if (isTimerRunning)
+            {
+                StartButton_Click(sender, e);
+            }
+            remainingTime = 0;
+            setTime = 0;
+            UpdateTimer () ;
+            TimerSeterTextBox.Text = "00:00:00";
         }
     }
 }
